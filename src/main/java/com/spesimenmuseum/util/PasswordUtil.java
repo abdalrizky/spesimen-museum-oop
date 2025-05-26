ea553b1d5d7a7f7176e5c735dfc629ad3729aed9
Muhammad Abdal Rizky <rizky.muhammad425@gmail.com>
2025-05-27 00:01:00 +0800

    add bcrypt password hash util

---
+++ src/main/java/com/spesimenmuseum/util/PasswordUtil.java
@@ -0,0 +1,13 @@
+package com.spesimenmuseum.util;
+
+import at.favre.lib.crypto.bcrypt.BCrypt;
+
+public class PasswordUtil {
+    public static String hashPassword(String plainTextPassword) {
+        return BCrypt.withDefaults().hashToString(12, plainTextPassword.toCharArray());
+    }
+
+    public static boolean checkPassword(String plainTextPassword, String hashedPassword) {
+        return BCrypt.verifyer().verify(hashedPassword.toCharArray(), plainTextPassword.toCharArray()).verified;
+    }
+}
