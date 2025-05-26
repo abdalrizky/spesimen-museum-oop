7ee7be217fab015afc887241ca3eafff0ef5bfe3
Muhammad Abdal Rizky <rizky.muhammad425@gmail.com>
2025-05-27 00:15:42 +0800

    add methods for coloring output

--- src/main/java/com/spesimenmuseum/util/ConsoleUtil.java
+++ src/main/java/com/spesimenmuseum/util/ConsoleUtil.java
@@ -5,4 +5,20 @@ public class ConsoleUtil {
         System.out.print("\033[H\033[2J");
         System.out.flush();
     }
+
+    public static void blueColor() {
+        System.out.print("\033[0;34m");
+    }
+
+    public static void redColor() {
+        System.out.print("\033[0;31m");
+    }
+
+    public static void greenColor() {
+        System.out.print("\033[0;32m");
+    }
+
+    public static void resetColor() {
+        System.out.print("\033[0m");
+    }
 }
